169dec1a0941356d310114c310f6810a43352d60
Tyrone Taylor <28680107+ttaylor92@users.noreply.github.com>
2025-01-26 20:10:24 -0500

    Refactor ResolverBucket to use ETS and GenServer for improved performance

--- lib/graphql_api_assignment/resolver_bucket.ex
+++ lib/graphql_api_assignment/resolver_bucket.ex
@@ -1,21 +1,42 @@
 defmodule GraphqlApiAssignment.ResolverBucket do
-  use Agent
+  use GenServer
 
   @default_name __MODULE__
+  @table_name :bucket
 
   # API
   def start_link(opts \\ []) do
-    initial_state = %{}
+    table_name = Keyword.get(opts, :table_name, @table_name)
     opts = Keyword.put_new(opts, :name, @default_name)
-    Agent.start_link(fn -> initial_state end, opts)
+    GenServer.start(__MODULE__, %{table_name: table_name}, opts)
   end
 
   # API
   def increment_key(key, name \\ @default_name) do
-    Agent.update(name, fn state -> Map.update(state, key, 1, &(&1 + 1)) end)
+    GenServer.cast(name, key)
   end
 
   def get_key_count(key, name \\ @default_name) do
-    Agent.get(name, &Map.get(&1, key, 0))
+    GenServer.call(name, key)
+  end
+
+  @impl true
+  def init(%{table_name: table_name}) do
+    :ets.new(table_name, [:named_table, read_concurrency: true])
+    {:ok, %{table_name: table_name}}
+  end
+
+  @impl true
+  def handle_call(key, _from, state) do
+    case :ets.lookup(state.table_name, key) do
+      [{^key, count}] -> {:reply, count, state}
+      [] -> {:reply, 0, state}
+    end
+  end
+
+  @impl true
+  def handle_cast(key, state) do
+    :ets.update_counter(state.table_name, key, {2, 1}, {key, 0})
+    {:noreply, state}
   end
 end
--- test/graphql_api_assignment/resolver_bucket_test.exs
+++ test/graphql_api_assignment/resolver_bucket_test.exs
@@ -4,38 +4,41 @@ defmodule GraphqlApiAssignment.ResolverBucketTest do
   alias GraphqlApiAssignment.ResolverBucket
 
   describe "incrementing user action counts" do
-    setup do
-      gen_server_name = nil
-      {:ok, pid} = ResolverBucket.start_link(name: gen_server_name)
+    test "increments create_user count" do
+      {:ok, pid} = ResolverBucket.start_link(name: :create_user_bucket, table_name: :create_user_table)
 
-      %{pid: pid}
-    end
-
-    test "increments create_user count", %{pid: pid} do
       assert ResolverBucket.get_key_count(:create_user, pid) === 0
       ResolverBucket.increment_key(:create_user, pid)
       assert ResolverBucket.get_key_count(:create_user, pid) === 1
     end
 
-    test "increments update_user count", %{pid: pid} do
+    test "increments update_user count" do
+      {:ok, pid} = ResolverBucket.start_link(name: :update_user_bucket, table_name: :update_user_table)
+
       assert ResolverBucket.get_key_count(:update_user, pid) === 0
       ResolverBucket.increment_key(:update_user, pid)
       assert ResolverBucket.get_key_count(:update_user, pid) === 1
     end
 
-    test "increments update_user_preferences count", %{pid: pid} do
+    test "increments update_user_preferences count" do
+      {:ok, pid} = ResolverBucket.start_link(name: :update_user_preferences_bucket, table_name: :update_user_preferences_table)
+
       assert ResolverBucket.get_key_count(:update_user_preferences, pid) === 0
       ResolverBucket.increment_key(:update_user_preferences, pid)
       assert ResolverBucket.get_key_count(:update_user_preferences, pid) === 1
     end
 
-    test "increments get_user count", %{pid: pid} do
+    test "increments get_user count" do
+      {:ok, pid} = ResolverBucket.start_link(name: :get_user_bucket, table_name: :get_user_table)
+
       assert ResolverBucket.get_key_count(:get_user, pid) === 0
       ResolverBucket.increment_key(:get_user, pid)
       assert ResolverBucket.get_key_count(:get_user, pid) === 1
     end
 
-    test "increments get_users count", %{pid: pid} do
+    test "increments get_users count" do
+      {:ok, pid} = ResolverBucket.start_link(name: :get_users_bucket, table_name: :get_users_table)
+
       assert ResolverBucket.get_key_count(:get_users, pid) === 0
       ResolverBucket.increment_key(:get_users, pid)
       assert ResolverBucket.get_key_count(:get_users, pid) === 1
